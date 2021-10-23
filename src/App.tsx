c13e3f9b0ad43dc8b168bd497d00c8331218c2b6
Bruno Gonçalves <bcgonca@gmail.com>
2021-10-23 09:36:37 -0300

    Import compose

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,29 @@
 import React, { ChangeEvent, FC, useState } from 'react';
 import './App.css';
-import TodoList from './Components/TodoList';
+import Compose from './Components/Compose';
+import List from './Components/List';
 
 interface ITask {
-  taskName: string,
-  deadline: number
+  taskName: string
 }
 
 const App: FC = () => {
 
   const [task, setTask] = useState<string>('')
   const [todo, setTodo] = useState<ITask[]>([])
-  const [deadline, setDeadline] = useState<number>(0)
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     if (event.target.name === 'task') {
       setTask(event.target.value)
     }
-
-    if (event.target.name === 'priority') {
-      setDeadline(Number(event.target.value))
-    }
   }
 
   const saveTask = (): void => {
     const newTask = {
-      taskName: task,
-      deadline: deadline
+      taskName: task
     }
     setTodo([...todo, newTask])
     setTask('')
-    setDeadline(0)
   }
 
   const deleteTask = (name: string): void => {
@@ -47,33 +40,21 @@ const App: FC = () => {
         <div className="header">
 
           <div className="header-title">
-            <h1>Página Inicial</h1>
+            <h1>Task</h1>
           </div>
 
-          <div className="input-group">
-            <input
-              type="text"
-              placeholder="New task..."
-              name="task"
-              className="form-control"
-              value={task}
-              onChange={handleChange}
-            />
-            <button
-              type="submit"
-              className="btn btn-primary"
-              onClick={saveTask}
-            >Save Task</button>
-          </div>
+          <Compose
+            task={task}
+            onChange={handleChange}
+            onSave={saveTask}
+          />
 
         </div>
         <div className="list">
 
-          <h2 className="list-title">Tasks</h2>
-
           {todo.map((task: ITask, key: number) => {
             return (
-              <TodoList
+              <List
                 task={task}
                 key={key}
                 deleteTask={deleteTask}
