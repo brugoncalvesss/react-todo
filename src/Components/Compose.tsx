caabac7a154be6942815f85281af5180f18121da
Bruno Gonçalves <bcgonca@gmail.com>
2021-10-23 09:33:26 -0300

    Feat: Compose todo task

---
+++ src/Components/Compose.tsx
@@ -0,0 +1,33 @@
+import React, { ChangeEvent } from 'react'
+
+interface ComposeProps {
+  task: string,
+  onChange(event: ChangeEvent<HTMLInputElement>): void,
+  onSave(): void
+}
+
+const Compose = ({task, onChange, onSave}: ComposeProps) => {
+
+  return (
+    <div>
+      <div className="input-group">
+        <input
+          type="text"
+          placeholder="New task..."
+          name="task"
+          className="form-control"
+          value={task}
+          onChange={onChange}
+        />
+
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={onSave}
+        >Save Task</button>
+      </div>
+    </div>
+  )
+}
+
+export default Compose
